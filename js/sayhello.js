2b4cb3d475af05708a926aee103f67e887abe29c
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-01-25 22:16:32 +0100

    zakomentowanie alertu + 2 inne koment.

---
+++ js/sayhello.js
@@ -0,0 +1,9 @@
+var name = prompt('Wprowadz swoje imie');
+// alert przestal sie wyswietlac alert('Siemka, ' + name + '!');
+console.log('Siemka, ' + name +'!');
+// to jest komentarz liniowy
+
+/* to
+jest
+komentarz
+blokowy */
